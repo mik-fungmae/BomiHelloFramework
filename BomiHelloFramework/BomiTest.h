dee8d62911a02920972ac8a1ab27c1422ed604b0
wuuhaokun <w.sirius@gmail.com>
2019-11-01 16:23:21 +0800

    ok

---
+++ BomiHelloFramework/BomiTest.h
@@ -0,0 +1,17 @@
+//
+//  BomiTest.h
+//  BomiHelloFramework
+//
+//  Created by Jack on 2019/11/1.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BomiTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BomiHelloFramework/BomiTest.m
@@ -0,0 +1,13 @@
+//
+//  BomiTest.m
+//  BomiHelloFramework
+//
+//  Created by Jack on 2019/11/1.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+#import "BomiTest.h"
+
+@implementation BomiTest
+
+@end
