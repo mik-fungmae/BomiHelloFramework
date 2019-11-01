0a116c558cfddd6129f7e63e0f21f1fdeb6e46a1
wuuhaokun <w.sirius@gmail.com>
2019-11-01 15:37:44 +0800

    hello

---
+++ BomiHelloFramework/BomiHelloFramework.h
@@ -0,0 +1,19 @@
+//
+//  BomiHelloFramework.h
+//  BomiHelloFramework
+//
+//  Created by Jack on 2019/11/1.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BomiHelloFramework.
+FOUNDATION_EXPORT double BomiHelloFrameworkVersionNumber;
+
+//! Project version string for BomiHelloFramework.
+FOUNDATION_EXPORT const unsigned char BomiHelloFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BomiHelloFramework/PublicHeader.h>
+
+
